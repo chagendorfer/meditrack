ac1aee50ffe564abb82123a6569e30c899bac3f0
Catharina <catharina.hagendorfer@edu.fh-joanneum.at>
2025-06-13 20:23:18 +0200

    added MedicationDetailActivity

---
+++ app/src/main/java/at/fhj/chagendorfer/meditrack/activities/MedicationDetailActivity.java
@@ -0,0 +1,29 @@
+package at.fhj.chagendorfer.meditrack.activities;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MedicationDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_medication_detail);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.medication_detail_screen), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinnerDoseUnit);
+        String selectedUnit = spinner.getSelectedItem().toString();
+
+    }
+}
